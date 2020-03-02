4a1ea90222148ac5d73ed3cc527c3ced7c57c209
andykuo1 <andykuo1supergreen@gmail.com>
2020-03-02 07:56:12 -0800

    Added docs for GraphContext

--- src/services2/graph/GraphContext.jsx
+++ src/services2/graph/GraphContext.jsx
@@ -13,6 +13,23 @@ export const GraphTypeContext = React.createContext();
 export const GraphStateContext = React.createContext();
 export const GraphDispatchContext = React.createContext();
 
+/**
+ * To use this, you need 1 thing:
+ * - the GraphType
+ * 
+ * The GraphType is simply an object (or a class with static properties)
+ * that has an array of used "elementTypes" and an async "reducer" function.
+ * The "reducer" is used by any behaviors or actions performed on the graph
+ * elements themselves. The "elementTypes" is primarily used by the serializer
+ * to identify all used elements of the graph.
+ * 
+ * You can also optionally provide it with the initial state of the "graphState".
+ * Any time this "graphState" changes through props, the entire graph will be reset
+ * to that state.
+ * 
+ * @param {React.Props} props The component props.
+ * @returns {React.ReactNode} The rendered node.
+ */
 export function GraphProvider(props)
 {
     const { graphType, graphState } = props;
@@ -67,6 +84,12 @@ GraphStateProvider.defaultProps = {
     initialState: {},
 };
 
+/**
+ * If you ever need an old-fashioned context consumer...
+ * 
+ * @param {React.Props} props The component props.
+ * @returns {React.ReactNode} The rendered node.
+ */
 export function GraphConsumer(props)
 {
     return (
