6a9559dbcb5ef53a55e33ef93b3a33b6e5c4cb91
Jacob Chazen <jchazen@gmail.com>
2020-04-24 16:29:47 -0700

    Added icons for string tester

--- src/modules/fa/drawer/testing/StringTester.jsx
+++ src/modules/fa/drawer/testing/StringTester.jsx
@@ -1,132 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import Options from '@flapjs/components/options/Options.jsx';
 
 import FieldButton from '@flapjs/components/lib/FieldButton.jsx';
 import Button from '@flapjs/components/lib/Button.jsx';
-//import { solveFSA } from '@flapjs/modules/fa/machine/FSAUtils.js';
-//import { useMachine } from '@flapjs/services/machine/MachineHooks';
-//import { checkPropTypes } from 'prop-types';
-
-
-/*
-class Parent
-{
-    render()
-    {
-        const componnetInstance;//...
-
-
-        componnetInstance.state.asdf
-        return (
-            <Component asdf="hello">
-
-            </Component>
-        );
-    }
-}
-
-class Component extends React.Component {
-
-    constructor(props)
-    {
-        super(props);
-
-        props.asdf // hello
-
-        this.state = {
-            asdf: props.asdf
-        };
-
-        this.onClick = this.onClick.bind('ghello');
-    }
-
-    onClick()
-    {
-        this.state.asdf
-    }
-
-    render()
-    {
-        this.state.dfasdfasdf
-        this.setState({ stadfaf: 'hello' });
-
-        function onClick()
-        {
-            this.skldfjlaskdf
-        }
-        ionClikc.bind(this);
-
-        const onclick = () => {
-            this.sdhfhd
-        }
-
-        return (
-            <div className="hello" onClick={onClick}>
-            </div>
-        );
-    }
-}
-
-function Component(props)
-{
-    const [asdf, setAsdf] = useState('hello');
-
-    function onClick()
-    {
-
-    }
-
-    return (
-        <div>
-
-        </div>
-    );
-}
-*/
+import { solveFSA } from '@flapjs/modules/fa/machine/FSAUtils.js';
+import { useMachine } from '@flapjs/services/machine/MachineHooks.jsx';
+//import { CircleCheckIcon, CircleCrossIcon } from '@flapjs/components/icons/Icons.js';
+import { EMPTY_SYMBOL } from '@flapjs/modules/fa/machine/Symbols.js';
 
 export default function StringTester(props)
 {
-    // const [ isAccepted, setAccepted ] = useState(null);
-    const isAccepted = false;
-    const [tests, updateTests] = useState([]);
-    //const FSA = useMachine(props.machineBuilderType, props.machineName);
+    const [testStrings, setTestStrings] = useState([]);
+    const [testResults, setTestResults] = useState([]);
+    const fsa = useMachine(props.machineBuilderType, props.machineName);
+
+    const onNewTestString = useCallback(() =>
+    {
+        setTestStrings([...testStrings, '']);
+        setTestResults([...testResults, 'none']);
+    },
+    [testStrings, setTestStrings, testResults, setTestResults]);
 
     return (
         <Options title="String Tester">
             <div>
-                <FieldButton id="testStringButton" onClick={() => createNewTest(tests, updateTests)}>
-                    Test String
+                <FieldButton id="testStringButton"
+                    onClick={onNewTestString}>
+                    Add Test String
                 </FieldButton>
 
-                {tests.length > 0 && <Button>Run All</Button>}
-                {tests.map((value, key) => (
-                    <p key={key}>
-                        <Button>
-                            Run
-                        </Button>
-                        <input type="text"
-                            id={'testString' + key}
-                            value={value}
-                            onChange={e =>
-                            {
-                                const newTests = [...tests];
-                                newTests[key] = e.target.value;
-                                updateTests(newTests);
-                            }} />
-                    </p>
-                ))}
-
-                <output>
-                    <p>
-                        {isAccepted === null
-                            ? '-- ??? --'
-                            : isAccepted
-                                ? '-- Accepted --'
-                                : '-- Rejected --'}
-                    </p>
-                </output>
+                <Button disabled={testStrings.length <= 0}
+                    onClick={() =>
+                    {
+                        let results = [];
+                        for(let i = 0; i < testStrings.length; i++)
+                        {
+                            let result = solveFSA(fsa, testStrings[i]);
+                            results.push(result ? 'pass' : 'fail');
+                        }
+                        setTestResults(results);
+                    }}>
+                    Run All
+                </Button>
+                {testStrings.map((value, key) =>
+                    <TestString
+                        key={key}
+                        value={value}
+                        result={testResults[key]}
+                        onClick={e =>
+                        {
+                            let newTestResults = [...testResults];
+                            newTestResults[key] = solveFSA(fsa, value) ? 'pass' : 'fail';
+                            setTestResults(newTestResults);
+                        }}
+                        onChange={e =>
+                        {
+                            const newTests = [...testStrings];
+                            newTests[key] = e.target.value;
+                            setTestStrings(newTests);
+                        }}
+                    />
+                )}
             </div>
         </Options>
     );
@@ -138,11 +74,51 @@ StringTester.propTypes = {
 StringTester.defaultProps = {
 };
 
-
-function createNewTest(tests, updateTests)
+function TestString(props)
 {
-    updateTests([...tests, '']);
+    const { value, result, onClick, onChange } = props;
+     
+    let renderedStatus;
+    switch(result)
+    {
+        case 'pass':
+            renderedStatus = '\u2611';
+            break;
+        case 'fail':
+            renderedStatus = '\u2612';
+            break;
+        case 'none':
+            renderedStatus = '\u2610';
+            break;
+        default:
+            throw new Error(`Unknown status '${result}'.`);
+    }
+
+    return (
+        <p>
+            <Button onClick={onClick}>
+                Run
+            </Button>
+            <input type="text"
+                value={value}
+                placeholder={EMPTY_SYMBOL}
+                onChange={onChange} />
+            <label style={{ fontSize: '1.5rem' }}>
+                {renderedStatus}
+            </label>
+        </p>
+    );
 }
-
-
-
+TestString.propTypes = {
+    value: PropTypes.string.isRequired,
+    result: PropTypes.oneOf([
+        'pass',
+        'fail',
+        'none',
+    ]),
+    onClick: PropTypes.func,
+    onChange: PropTypes.func,
+};
+TestString.defaultProps = {
+    result: null,
+};
