345579f7003284cc5fbd81f190b2286b6a947fe1
Andrew Kuo <andykuo1supergreen@gmail.com>
2020-08-28 21:44:44 -0700

    Removed Toast

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -15,22 +15,7 @@ import DebugToggle from './DebugToggle.jsx';
 import IconButton from './icons/IconButton.jsx';
 import { BugIcon } from './icons/Icons.js';
 
-import { ToastProvider, useToasts } from '@flapjs/services/toast/ToastProvider.jsx';
-
 const BUGREPORT_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfqBFiGFGnxPI7QIWscv8jsIr5bj4LA3CS-is__2-YvJ_kTjQ/viewform';
-
-function Toaster()
-{
-    const { add } = useToasts();
-    return (
-        <button onClick={() =>
-        {
-            add('Woot!');
-        }}>
-            AddToast
-        </button>
-    );
-}
 
 /**
  * Defines these slots:
@@ -50,44 +35,41 @@ export default function AppLayout(props)
     return (
         <>
             <Slot mode="wrapped" name="providers">
-                <ToastProvider>
-                    <header>
-                        <Slot name="header"></Slot>
-                        <AppBar>
-                            <Toaster></Toaster>
-                            <Logo title="Flap.js" version={app.version}/>
-                            <DebugToggle/>
-                            <ModuleSelector/>
-                            <Slot name="appbar"></Slot>                    
-                            <IconButton iconClass= { BugIcon } onClick={() => window.open(BUGREPORT_URL, '_blank')}> </IconButton>
-                        </AppBar>
-                    </header>
-                    <main>
-                        <Slot mode="consumer" name="drawer">
-                            {slots => (
-                                <Workspace
-                                    renderBackground={() => (
-                                        <Viewport>
-                                            <Slot name="background"></Slot>
-                                        </Viewport>
-                                    )}
-                                    renderForeground={() => (
-                                        <Viewport>
-                                            <Slot name="foreground"></Slot>
-                                        </Viewport>
-                                    )}
-                                    panels={slots}>
-                                    <Slot name="workspace"></Slot>
-                                </Workspace>
-                            )}
-                        </Slot>
-                    </main>
-                    <footer>
-                        {/* Nothing yet. Perhaps ice cream? */}
-                        <Slot name="footer"></Slot>
-                    </footer>
-                    <Slot name="splash"></Slot>
-                </ToastProvider>
+                <header>
+                    <Slot name="header"></Slot>
+                    <AppBar>
+                        <Logo title="Flap.js" version={app.version}/>
+                        <DebugToggle/>
+                        <ModuleSelector/>
+                        <Slot name="appbar"></Slot>                    
+                        <IconButton iconClass= { BugIcon } onClick={() => window.open(BUGREPORT_URL, '_blank')}> </IconButton>
+                    </AppBar>
+                </header>
+                <main>
+                    <Slot mode="consumer" name="drawer">
+                        {slots => (
+                            <Workspace
+                                renderBackground={() => (
+                                    <Viewport>
+                                        <Slot name="background"></Slot>
+                                    </Viewport>
+                                )}
+                                renderForeground={() => (
+                                    <Viewport>
+                                        <Slot name="foreground"></Slot>
+                                    </Viewport>
+                                )}
+                                panels={slots}>
+                                <Slot name="workspace"></Slot>
+                            </Workspace>
+                        )}
+                    </Slot>
+                </main>
+                <footer>
+                    {/* Nothing yet. Perhaps ice cream? */}
+                    <Slot name="footer"></Slot>
+                </footer>
+                <Slot name="splash"></Slot>
             </Slot>
         </>
     );
