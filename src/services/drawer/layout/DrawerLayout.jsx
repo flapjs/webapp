1e876d847f1f240e77edf7893da2ad6d569a3c61
andykuo1 <andykuo1supergreen@gmail.com>
2020-09-08 21:07:48 -0700

    Added DrawerLayout docs

--- src/services/drawer/layout/DrawerLayout.jsx
+++ src/services/drawer/layout/DrawerLayout.jsx
@@ -4,9 +4,18 @@ import Style from './DrawerLayout.module.css';
 
 import * as SideHelper from '@flapjs/components/sidebar/SideHelper.js';
 
-// Out of 100%
+/** The drawer container size. Out of 100 (of the window size). */
 const INITIAL_DRAWER_SIZE = 30;
 
+/**
+ * # Snap Behavior - QoL feature
+ * - When dragging the drawer handle, it can "jump to" a nearby position while dragging
+ * to suggest to the user, or for ease of access, a useful dimension.
+ */
+
+/**
+ * A component to layout and position the drawer container and content.
+ */
 export default class DrawerLayout extends React.Component
 {
     constructor(props)
@@ -26,17 +35,28 @@ export default class DrawerLayout extends React.Component
         this.onDrawerHandleDragEnd = this.onDrawerHandleDragEnd.bind(this);
     }
 
+    /**
+     * A callback to handle the start of the drawer handle drag logic. This is passed
+     * to the handle element as an event listener for 'mousedown'. This initializes the
+     * handle logic sequence, attaching {@link onDrawerHandleDragMove} and {@link onDrawerHandleDragEnd}.
+     * 
+     * @param {MouseEvent} e The event object from the mouse event.
+     */
     onDrawerHandleDragBegin(e)
     {
         document.addEventListener('mousemove', this.onDrawerHandleDragMove);
         document.addEventListener('mouseup', this.onDrawerHandleDragEnd);
     }
 
+    /**
+     * A callback to handle the updates of the drawer handle drag logic. This is passed
+     * to the document as an event listener for 'mousemove'. This is removed by {@link onDrawerHandleDragEnd}.
+     * 
+     * @param {MouseEvent} e The event object from the mouse event.
+     */
     onDrawerHandleDragMove(e)
     {
-        const side = this.props.side;
-        const snapPoints = this.props.snapPoints;
-        const snapBehavior = this.props.snapBehavior;
+        const { side, snapPoints, snapBehavior } = this.props;
         const size = this.state.size;
         const boundingRect = this.container.current.getBoundingClientRect();
 
@@ -84,6 +104,13 @@ export default class DrawerLayout extends React.Component
         }
     }
 
+    /**
+     * A callback to handle the end of the drawer handle drag logic (basically cleanup).
+     * This is passed to the document as an event listener for 'mouseup'. This is removed
+     * by itself.
+     * 
+     * @param {MouseEvent} e The event object from the mouse event.
+     */
     onDrawerHandleDragEnd(e)
     {
         document.removeEventListener('mousemove', this.onDrawerHandleDragMove);
@@ -100,11 +127,13 @@ export default class DrawerLayout extends React.Component
 
         const horizontal = SideHelper.isHorizontal(side);
 
+        // Determine the size of the drawer given whether it is laid out vertically vs horizontally.
         const drawerStyle = {
             width: horizontal ? `${size}%` : '100%',
             height: !horizontal ? `${size}%` : '100%',
         };
         
+        // Side refers to which side of the screen the drawer extends out of.
         const containerFlexDirection = (
             side === 'left' ? 'row' :
                 side === 'right' ? 'row-reverse' :
@@ -122,13 +151,11 @@ export default class DrawerLayout extends React.Component
                 // @ts-ignore
                 style={containerStyle}>
                 <div ref={this.drawer}
-                    className={Style.drawer
-                        + ' ' + (className || '')
-                        + ' ' + side
-                        + (open ? ' open' : '')}
+                    className={`${Style.drawer} ${className || ''} ${side} ${open ? 'open' : ''}`}
                     style={drawerStyle}>
                     <div className={Style.handle}
                         role="presentation"
+                        // @ts-ignore
                         onMouseDown={this.onDrawerHandleDragBegin}>
                     </div>
                     <div className={Style.content}>
@@ -166,6 +193,16 @@ DrawerLayout.defaultProps = {
     snapBehavior: 'range',
 };
 
+/**
+ * Handles the rendering of the drawer layout child. This is
+ * either a function call (like a consumer), or an actual rendered node.
+ * 
+ * @param {Function|*} drawer The drawer layout child (the content).
+ * @param {String} side The side the drawer is on.
+ * @param {Boolean} open Whether the drawer is open.
+ * @param {Number} size The size of the drawer.
+ * @returns {import('react').ReactNode} The rendered drawer content.
+ */
 function renderDrawerContent(drawer, side, open, size)
 {
     if (typeof drawer === 'function')
@@ -182,6 +219,15 @@ function renderDrawerContent(drawer, side, open, size)
     }
 }
 
+/**
+ * Helper function to get the snap-point given the current position and range.
+ * 
+ * @param {Number} value The current position of the drawer.
+ * @param {Array<Number>} snapPoints The list of possible snap points.
+ * @param {Number} [maxValue=100] The maximum position possible for the drawer.
+ * @param {Number} [range=15] The buffer around the snap point before snapping.
+ * @returns {Number} The new calculated position for the drawer.
+ */
 function applySnapPointsWithinRange(value, snapPoints, maxValue = 100, range = 15)
 {
     for(const snapPoint of snapPoints)
@@ -194,6 +240,13 @@ function applySnapPointsWithinRange(value, snapPoints, maxValue = 100, range = 1
     return value;
 }
 
+/**
+ * Helper function to get the snap-point given the current position (by proximity).
+ * 
+ * @param {Number} value The current position of the drawer.
+ * @param {Array<Number>} snapPoints The list of possible snap points.
+ * @returns {Number} The new calculated position for the drawer.
+ */
 function applySnapPointsNearest(value, snapPoints)
 {
     let minValue = value;
