6d872e7e3642527f018525dd416008c5eb848a3e
ppaleja <46587956+ppaleja@users.noreply.github.com>
2020-11-23 22:12:28 -0800

    skeleton for last part of preprocessing, still not tested -pawan

--- docs/deprecated/app/modules/pda/machine/util/converntPDA.js
+++ docs/deprecated/app/modules/pda/machine/util/converntPDA.js
@@ -1,6 +1,10 @@
 import { State } from '../PDA';
 
-export const EMPTY_SYMBOL = '&empty';
+export const EMPTY_SYMBOL = '&empty'; // Why do we need to export here?
+const READ_SYMBOL_INDEX = 1;
+const TO_STATE_INDEX = 2;
+const POP_SYMBOL_INDEX = 3;
+const PUSH_SYMBOL_INDEX = 4;
 
 
 export function convertPDA(pda)
@@ -9,7 +13,7 @@ export function convertPDA(pda)
 //     Preprocessing
 
 // 1. Make all accept states empty the stack (create loop)
-const pdaCopy = copy(pda);        //Copy state (might need to fix copy function)
+const pdaCopy = copy(pda);        //Copy pda is shallow, so this doesn't really do anything.
 //make a start state to add a stack symbol on stack with epsilon transition
 //for transition symbol just concatenate all string from alphabet
 //add a epsilon transitoin to old start state
@@ -57,17 +61,49 @@ pdaCopy.addTransition(
 //convert the state to non accept
 
 // TODO: Not correct, would need to make sure that the computation would have stayed in this accept state
-// when we ran out of character.
+// when we ran out of character. Edit: I think we're good here actually, because the accept state will only accept
+// with no characters left.
 for(const s of pda._finalStates.entries()) {
     pdaCopy.addTransition(
-    s, 
-    clear, 
+        s, 
+        clear, 
+        EMPTY_SYMBOL, 
+        EMPTY_SYMBOL,
+        EMPTY_SYMBOL // Added our special stack symbol
+    );     
+}
+pdaCopy.addTransition( 
+    clear,
+    newAcceptState,
     EMPTY_SYMBOL, 
     EMPTY_SYMBOL,
     EMPTY_SYMBOL // Added our special stack symbol
-);      
+);
 
+// So now we need to loop through all transitions
+
+inter = new State('Inter'); // So for all these custom states, we have to make sure that one doesn't already exist.
+
+// Instead we're going to loop through all the outgoing transitions of a state
+for(const [stateId, state] of pdaCopy._states.entries()) {
+    const dst = state.getOutgoingTransitions(state); // dst = [state, readSymbol, destState, popSymbol, pushSymbol]
+    for(const tran of dst) {
+        inter = new State('Inter' + /*Some unique string dependent on count or something */); // 
+        // Case that both are push
+        if(tran[POP_SYMBOL_INDEX] === EMPTY_SYMBOL && tran[PUSH_SYMBOL_INDEX] === EMPTY_SYMBOL) {
+            // We want to create a new transition from state to inter that pushes an arbitrary stack symbol
+            
+            // Then from inter to deststate we pop that symbol
+
+        } else if(tran[POP_SYMBOL_INDEX] !== EMPTY_SYMBOL && tran[PUSH_SYMBOL_INDEX] === EMPTY_SYMBOL) {
+            // We want to create a new transition from state to inter that pops the pop symbol
+
+            // And a new transition from inter to deststate that pushes the push symbol
+        }
     }
+}
+
+
 //transition (state, alphabet symbol, push to stack) == (state, pop off stack)
 // if transistion(p, a, \epsilon == (r, u) and transition(s, b, u) == (q, \epsilon)
 // add rule (p, q) → a (r,s) b to G
